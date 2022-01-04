1c159d3ee3f23fd99f9b8b5ef8f1a8927779cf34
Anil0446 <96903353+Anil0446@users.noreply.github.com>
2022-01-04 16:06:00 +0530

    Initial commit

---
+++ getandpostapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def getinput(request):
+    return render(request, 'getinput.html')
+
+def getadd(request):
+    x = request.GET["t1"]
+    y = request.GET["t2"]
+    z = (int(x)+int(y))
+    res = HttpResponse("sum is:"+str(z))
+    return res
+
+
+def postinput(request):
+    return render(request, 'postinput.html')
+
+def postadd(request):
+    x = request.POST["t1"]
+    y = request.POST["t2"]
+    z = int(x) + int(y)
+    res = HttpResponse("sum is:"+str(z))
+    return res
+
+
+# Create your views here.
